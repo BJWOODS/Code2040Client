0a5eab806e9de44ee7f39391f9966d9fbb14de68
BJWOODS <bwoodard@csumb.edu>
2016-10-25 08:43:17 -0700

    Step 3 of Code2040 Challenge

--- Code2040.py
+++ Code2040.py
@@ -1,4 +1,5 @@
 import httplib, urllib
+import ast
 responseMsg = ""
 def WebClientApiCaller(host, path, jsonData): 
     params = urllib.urlencode(jsonData)
@@ -16,14 +17,24 @@ def WebClientApiCaller(host, path, jsonData):
 def reverseString(str): 
     revStr = str[::-1] #reversing string using slicing 
     return "".join(revStr)
+def needle(dictionary):
+    for value in dictionary["haystack"]:
+        if(value == dictionary["needle"]):
+            return dictionary["haystack"].index(value) #found the needle!
+
 
 path = "/api/haystack?token=9e46e028f978939349e3fcd8d8a8c283&github=github.com/BJWOODS/Code2040Client"
 validPath = "/api/haystack/validate"
 host = "challenge.code2040.org"
 jsonData = {"token":"9e46e028f978939349e3fcd8d8a8c283", "github":"github.com/BJWOODS/Code2040Client"}
 responseMsg = WebClientApiCaller(host,path,jsonData)
+dictionary = ast.literal_eval(responseMsg) 
+#print dictionary
+#print responseMsg
+pos = needle(dictionary)
 
-print responseMsg
-jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283","needle":0}
+print pos
+
+jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283","needle":pos}
 
 WebClientApiCaller(host, validPath, jsonDataNew)
