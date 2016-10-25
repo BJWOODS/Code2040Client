14c792462c2026c6e2415de0d2203dff4703928d
BJWOODS <bwoodard@csumb.edu>
2016-10-25 06:11:29 -0700

    Step 3 of Code2040 Challenge

--- Code2040.py
+++ Code2040.py
@@ -8,7 +8,7 @@ def WebClientApiCaller(host, path, jsonData):
     conn.request("POST", path, params)
     response = conn.getresponse()
 
-    responseMsg = list(response.read())
+    responseMsg = response.read()
     print response.status
     return responseMsg
 
@@ -17,13 +17,13 @@ def reverseString(str):
     revStr = str[::-1] #reversing string using slicing 
     return "".join(revStr)
 
-path = "/api/reverse?token=9e46e028f978939349e3fcd8d8a8c283&github=github.com/BJWOODS/Code2040Client"
-validPath = "/api/reverse/validate?token=9e46e028f978939349e3fcd8d8a8c283&github=github.com/BJWOODS/Code2040Client"
+path = "/api/haystack?token=9e46e028f978939349e3fcd8d8a8c283&github=github.com/BJWOODS/Code2040Client"
+validPath = "/api/haystack/validate"
 host = "challenge.code2040.org"
 jsonData = {"token":"9e46e028f978939349e3fcd8d8a8c283", "github":"github.com/BJWOODS/Code2040Client"}
 responseMsg = WebClientApiCaller(host,path,jsonData)
 
-#print reverseString(responseMsg)
-jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283","string":reverseString(responseMsg)}
+print responseMsg
+jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283","needle":0}
 
 WebClientApiCaller(host, validPath, jsonDataNew)
