4555b5fdca78844c409d16e6b2bf0112be8c29fe
BJWOODS <bwoodard@csumb.edu>
2016-10-29 18:17:29 -0700

    Step 4 Code2040

--- Code2040.py
+++ Code2040.py
@@ -6,7 +6,7 @@ responseMsg = ""
 def WebClientApiCaller(host, path, jsonData): 
     params = urllib.urlencode(jsonData)
     #HTTP to bypass ssl
-    conn = httplib.HTTPConnection(host) #establishing connection
+    conn = httplib.HTTPConnection(host) #establishing connection in entire function 
     conn.connect()
     conn.request("POST", path, params)
     response = conn.getresponse()
@@ -24,16 +24,14 @@ def excludePrefix(dictionary):
         if(value[0:4] != dictionary["prefix"]):
             finalList.append(value)
 
-    #print finalList
     return finalList
+
 path = "/api/prefix"
 validPath = "/api/prefix/validate"
 host = "challenge.code2040.org"
 jsonData = {"token":"9e46e028f978939349e3fcd8d8a8c283"}
-#responseMsg = WebClientApiCaller(host,path,jsonData)
-#print (dictionary["prefix"])
-#print (dictionary)
-r = requests.post('http://challenge.code2040.org/api/prefix',jsonData)
+
+r = requests.post('http://challenge.code2040.org/api/prefix',jsonData) #using requests to post
 print (r)
 print (r.json())
 myarray = excludePrefix(r.json())
@@ -44,12 +42,5 @@ payload =  {"token":"9e46e028f978939349e3fcd8d8a8c283","array":myarray}
 print (payload)
 headers = {'content-type':'application/json'}
 j =requests.post('http://challenge.code2040.org/api/prefix/validate',data = json.dumps(payload),headers = headers)
-#dictionary = ast.literal_eval(responseMsg) #Converting string to dictionary
 
 print (j)
-#newArray = excludePrefix(dictionary)
-#print (newArray)
-#newArray = json.dumps(newArray)
-#jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283", "array":newArray}
-#requests.POST('http://challenge.code2040.org/api/prefix/validate',json.dumps(newArray))
-#WebClientApiCaller(host,validPath,jsonDataNew)
