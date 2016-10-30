a4b980208ebbaf82417ad92eb6a288a6c6656a60
BJWOODS <bwoodard@csumb.edu>
2016-10-29 18:12:53 -0700

    Step 4 Code2040

--- Code2040.py
+++ Code2040.py
@@ -1,6 +1,7 @@
-import httplib, urllib
+#import httplib, urllib
 import ast
 import json
+import requests
 responseMsg = ""
 def WebClientApiCaller(host, path, jsonData): 
     params = urllib.urlencode(jsonData)
@@ -11,7 +12,7 @@ def WebClientApiCaller(host, path, jsonData):
     response = conn.getresponse()
 
     responseMsg = response.read()
-    print response.status
+    #print response.status
     return responseMsg
 
 def reverseString(str): 
@@ -23,18 +24,32 @@ def excludePrefix(dictionary):
         if(value[0:4] != dictionary["prefix"]):
             finalList.append(value)
 
-    print finalList
+    #print finalList
     return finalList
 path = "/api/prefix"
 validPath = "/api/prefix/validate"
 host = "challenge.code2040.org"
 jsonData = {"token":"9e46e028f978939349e3fcd8d8a8c283"}
-responseMsg = WebClientApiCaller(host,path,jsonData)
-dictionary = ast.literal_eval(responseMsg) #Converting string to dictionary
-print dictionary["prefix"]
-print dictionary
+#responseMsg = WebClientApiCaller(host,path,jsonData)
+#print (dictionary["prefix"])
+#print (dictionary)
+r = requests.post('http://challenge.code2040.org/api/prefix',jsonData)
+print (r)
+print (r.json())
+myarray = excludePrefix(r.json())
+print ('')
 
-newArray = excludePrefix(dictionary)
-print newArray
-jsonDataNew = json.dumps({"token":"9e46e028f978939349e3fcd8d8a8c283", "array":newArray})
-WebClientApiCaller(host,validPath,jsonDataNew)
+print (myarray)
+payload =  {"token":"9e46e028f978939349e3fcd8d8a8c283","array":myarray}
+print (payload)
+headers = {'content-type':'application/json'}
+j =requests.post('http://challenge.code2040.org/api/prefix/validate',data = json.dumps(payload),headers = headers)
+#dictionary = ast.literal_eval(responseMsg) #Converting string to dictionary
+
+print (j)
+#newArray = excludePrefix(dictionary)
+#print (newArray)
+#newArray = json.dumps(newArray)
+#jsonDataNew = {"token":"9e46e028f978939349e3fcd8d8a8c283", "array":newArray}
+#requests.POST('http://challenge.code2040.org/api/prefix/validate',json.dumps(newArray))
+#WebClientApiCaller(host,validPath,jsonDataNew)
