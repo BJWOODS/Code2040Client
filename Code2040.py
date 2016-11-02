7b66bcc10f8b95010cddb8ec92ac1f117049c606
BJWOODS <bwoodard@csumb.edu>
2016-11-02 02:20:23 +0100

    Step 5

--- Code2040.py
+++ Code2040.py
@@ -1,46 +1,36 @@
-#import httplib, urllib
-import ast
-import json
+#Utilizing Python 3
 import requests
-responseMsg = ""
-def WebClientApiCaller(host, path, jsonData): 
-    params = urllib.urlencode(jsonData)
-    #HTTP to bypass ssl
-    conn = httplib.HTTPConnection(host) #establishing connection in entire function 
-    conn.connect()
-    conn.request("POST", path, params)
-    response = conn.getresponse()
+import datetime
+import json
 
-    responseMsg = response.read()
-    #print response.status
-    return responseMsg
+API_TOKEN = '9e46e028f978939349e3fcd8d8a8c283'
+ENDPOINT1 = 'http://challenge.code2040.org/api/dating'
+ENDPOINT2 = 'http://challenge.code2040.org/api/dating/validate'
 
-def reverseString(str): 
-    revStr = str[::-1] #reversing string using slicing 
-    return "".join(revStr)
-def excludePrefix(dictionary):
-    finalList = []
-    for value in dictionary["array"]:
-        if(value[0:4] != dictionary["prefix"]):
-            finalList.append(value)
 
-    return finalList
+r = requests.post(ENDPOINT1, data = {'token':API_TOKEN})
+data = json.loads(r.text)
+datestamp = data['datestamp']
+num_of_seconds = data['interval']
 
-path = "/api/prefix"
-validPath = "/api/prefix/validate"
-host = "challenge.code2040.org"
-jsonData = {"token":"9e46e028f978939349e3fcd8d8a8c283"}
+date_stamp_array = datestamp.split('T') #excluding 'T' in the ISO format
+date = date_stamp_array[0]
+time = date_stamp_array[1]
+time = time.split('Z')[0] #Excluding 'Z'
+new_datestamp = date + ' ' + time
 
-r = requests.post('http://challenge.code2040.org/api/prefix',jsonData) #using requests to post
-print (r)
-print (r.json())
-myarray = excludePrefix(r.json())
-print ('')
+#formatting
 
-print (myarray)
-payload =  {"token":"9e46e028f978939349e3fcd8d8a8c283","array":myarray}
-print (payload)
-headers = {'content-type':'application/json'}
-j =requests.post('http://challenge.code2040.org/api/prefix/validate',data = json.dumps(payload),headers = headers)
+format = '%Y-%m-%d %H:%M:%S' 
+new_datetime_date = datetime.datetime.strptime(new_datestamp, format)
 
-print (j)
+
+mod_datestamp = new_datetime_date + datetime.timedelta(seconds = num_of_seconds) #sum of new datetime
+mod_datestamp = mod_datestamp.strftime(format)
+mod_datestamp = mod_datestamp.split(' ')
+final_mod_datestamp = mod_datestamp[0] + 'T' + mod_datestamp[1] + 'Z'
+
+
+p = requests.post(ENDPOINT2, data = {'token':API_TOKEN, 'datestamp':final_mod_datestamp})
+print(p.text)
+
